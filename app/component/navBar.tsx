8e306808c70d079319d432bdaa85c8bb4670fea2
Siddharth Gupta <siddharth55x@gmail.com>
2024-05-07 15:33:36 +0530

    optimized

--- app/component/navBar.tsx
+++ app/component/navBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     <div className="w-full">
       <div className="p-3 pb-3 lg:p-10 lg:pb-8 xl:p-10 xl:pb-8 md:p-5 ">
         <div className=" flex justify-between">
-          <h1 className="font-semibold md:text-2xl xl:text-3xl lg:text-3xl sm:text-sm ">
+          <h1 className="font-semibold text-3xl md:text-2xl xl:text-3xl lg:text-3xl sm:text-sm ">
             GetJwel Sellers
           </h1>
           <div className=" flex gap-5">
@@ -16,7 +16,7 @@ const NavBar = () => {
                 Why Us?
               </a>
             </button>
-            <button className="bg-[#EAE4C0] pt-2 pb-2 pl-4 pr-4 sm:pt-0 sm:pb-0  sm:pl-[0.3rem] sm:pr-[0.3rem]  sm:text-[0.5rem]">
+            <button className="hover: bg-[#EAE4C0] pt-2 pb-2 pl-4 pr-4 sm:pt-0 sm:pb-0  sm:pl-[0.3rem] sm:pr-[0.3rem]  sm:text-[0.5rem]">
               Dashboard
             </button>
             <button className="bg-[#EAE4C0] pt-2 pb-2 pl-4 pr-4 sm:pt-0 sm:pb-0  sm:pl-[0.3rem] sm:pr-[0.3rem]  sm:text-[0.5rem] ">
